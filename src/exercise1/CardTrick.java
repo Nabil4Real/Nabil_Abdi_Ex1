b54e46c43f91bd09efaf39c02366c6ed36dbeb33
Nabil A <abdina@sheridancollege.ca>
2023-02-02 00:33:05 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -62,21 +62,21 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Nabil, and I am excited to learn and grow as a developer.");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Always learn and grow. Try different things.");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Reading");
+        System.out.println("-- Coding");
+        System.out.println("-- Writing/Watching TV");
+        System.out.println("-- Swimming");
 
-        System.out.println();
+        System.out.println("Nabil Out......Bye");
         
     
     }
